82de386336656751d8a00ac21c69e9428de58760
Justin <jmcmillion89@icloud.com>
2026-01-27 06:38:31 -0500

    Add nav function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css'
+import { displayHome } from './home.js'
+import { displayMenu } from './menu.js'
+import { displayAbout } from './about.js'
+
+const header = (() => {
+    const headerDiv = document.querySelector('header')
+    headerDiv.textContent = 'Header'
+})()
+
+const nav = (() => {
+    const navDiv = document.querySelector('nav')
+    const navBar = [['Home', displayHome], ['Menu', displayMenu], ['About', displayAbout]]
+    navBar.forEach((item) => {
+        const newBtn = document.createElement('button')
+        newBtn.textContent = item[0]
+        newBtn.addEventListener('click', () => item[1]())
+        navDiv.appendChild(newBtn)
+    })
+})()
+
+displayHome()
