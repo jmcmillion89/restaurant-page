d2b7c7b4bc3f7f643f76d5365106a5799607bc6e
Justin <jmcmillion89@icloud.com>
2026-01-31 08:19:20 -0500

    Add new container named menu

--- src/menu.js
+++ src/menu.js
@@ -3,22 +3,25 @@ import { menuItems } from "./menuItems.js"
 export const displayMenu = () => {
     const contentDiv = document.querySelector('#content')
     contentDiv.textContent = ''
+    const menu = document.createElement('div')
+    menu.classList.add('menu')
+    contentDiv.appendChild(menu)
     menuItems.forEach((item) => {
         const newDiv = document.createElement('div')
         newDiv.classList.add('card')
         const foodImage = new Image(200, 150)
         foodImage.src = item.image
         newDiv.appendChild(foodImage)
-        const title = document.createElement('div')
+        const title = document.createElement('h3')
         title.textContent = item.name
         newDiv.appendChild(title)
-        const description = document.createElement('div')
+        const description = document.createElement('p')
         description.textContent = item.description
         newDiv.appendChild(description)
-        const cost = document.createElement('div')
-        cost.textContent = item.cost
-        newDiv.appendChild(cost)
+        const price = document.createElement('div')
+        price.textContent = item.price
+        newDiv.appendChild(price)
 
-        contentDiv.appendChild(newDiv)
+        menu.appendChild(newDiv)
     })
 }
